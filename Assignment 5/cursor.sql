a487bb52339db787ae206e216c0dbd7d8c8e0890
Jasmine <jomeke@post.harvard.edu>
2017-10-23 16:36:01 -0500

    Create cursor.sql

---
+++ Assignment 5/cursor.sql
@@ -0,0 +1,80 @@
+/*learning aid, cursors */
+
+DROP TABLE STUDENT CASCADE CONSTRAINTS;
+DROP TABLE TEST CASCADE CONSTRAINTS;
+
+CREATE TABLE STUDENT
+(
+    ID CHAR(4),
+    Name VARCHAR2(20),
+    
+    PRIMARY KEY (ID)
+    );
+    
+CREATE TABLE TEST
+(
+    ID      CHAR(4),
+    Exam    VARCHAR2(10),
+    Score   NUMBER(3,0)
+            CHECK (Score >= 0 AND Score <= 100),
+    PRIMARY KEY (ID, Exam),
+    
+    FOREIGN KEY (ID)
+        REFERENCES STUDENT(ID)
+);
+
+INSERT INTO STUDENT VALUES('8000', 'Marta');
+INSERT INTO STUDENT VALUES('2100', 'Julie');
+INSERT INTO STUDENT VALUES('7856', 'Crista');
+INSERT INTO STUDENT VALUES('1130', 'Yasmin');
+
+INSERT INTO TEST VALUES('8000', 'Midterm', 77);
+INSERT INTO TEST VALUES('7856', 'Final', 95);
+INSERT INTO TEST VALUES('2100', 'Midterm', 98);
+INSERT INTO TEST VALUES('1130', 'Final', 100);
+
+SELECT * FROM STUDENT;
+
+SELECT * FROM TEST;
+
+
+declare
+    studentName STUDENT.Name%type;
+    studentID STUDENT.ID%type;
+    /*cursor set up. We don't know state of the table yet*/
+    cursor studentPtr is SELECT * FROM STUDENT; 
+
+begin
+    dbms_output.put_line( 'Traversing STUDENT table with a cursor:'); 
+    
+    /*open cursor*/
+    open studentPtr;
+    
+    /*loop to fetch one record at a time until no more found*/
+    loop
+        fetch studentPtr into studentID, studentName;
+        
+        if studentPtr%found then /*if we have data, this will be true*/
+            dbms_output.put_line( studentPtr%rowcount || 
+            ' : For ID ' || studentID || ' , Name is ' || studentName);
+            
+        else
+            exit;
+        end if; 
+            
+    end loop; 
+    
+    close studentPtr;
+    
+    end;
+    
+    
+    
+    /* output
+    Traversing STUDENT table with a cursor:
+1 : For ID 8000 , Name is Marta
+2 : For ID 2100 , Name is Julie
+3 : For ID 7856 , Name is Crista
+4 : For ID 1130 , Name is Yasmin
+
+/*

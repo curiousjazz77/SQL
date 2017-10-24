3d5b25ca3ca67f2f806ee02629a27810eedc0e8d
Jasmine <jomeke@post.harvard.edu>
2017-10-23 19:50:20 -0500

    Create TestingTrigger2.sql

---
+++ Assignment 5/Triggers/TestingTrigger2.sql
@@ -0,0 +1,9 @@
+@/Users/.../Workers.sql
+
+INSERT INTO WORKERS VALUES('X', 4, 90000); 
+INSERT INTO WORKERS VALUES('Y', 1, 45000); 
+/*giving poeple a raise to everyone to check if still under 1 mil mimumimum*/ 
+UPDATE WORKERS SET Salary = Salary + 1000; 
+
+
+/*17 rows updated.*/

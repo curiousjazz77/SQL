86eb17ae55c395273d8974b5b446b68d44dbb1ba
Jasmine <jomeke@post.harvard.edu>
2017-10-23 19:56:25 -0500

    Update TestingTrigger2.sql

--- Assignment 5/Triggers/TestingTrigger2.sql
+++ Assignment 5/Triggers/TestingTrigger2.sql
@@ -4,6 +4,9 @@ INSERT INTO WORKERS VALUES('X', 4, 90000);
 INSERT INTO WORKERS VALUES('Y', 1, 45000); 
 /*giving poeple a raise to everyone to check if still under 1 mil mimumimum*/ 
 UPDATE WORKERS SET Salary = Salary + 1000; 
+DELETE FROM WORKERS WHERE DeptNumber = 5;
 
+/*output:
 
-/*17 rows updated.*/
+17 rows updated.
+3 rows deleted.*/

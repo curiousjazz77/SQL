53a21c43742697cd849c6aaebc56cd8dd45d75ac
Jazz <10506008+curiousjazz77@users.noreply.github.com>
2018-11-09 19:47:18 -0600

    Update between.sql

--- Sakila_Practice/between.sql
+++ Sakila_Practice/between.sql
@@ -13,3 +13,13 @@ order by payment_date;
 select amount, payment_date
 from payment
 where payment_date between '2007-02-07' and '2007-02-16';  --not inclusive of 2/16
+
+select count(*)
+from film
+where rating = 'R'
+and replacement_cost between 5 and 15 ; -- 52
+
+select count(*) 
+from film
+where rating = 'R'
+and replacement_cost >=5 and replacement_cost <= 15 ; --52

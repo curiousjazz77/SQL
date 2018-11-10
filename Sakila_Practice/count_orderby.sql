0645e498f2d012e8dfd883d5e6033c1bce3f164b
Jazz <10506008+curiousjazz77@users.noreply.github.com>
2018-11-09 19:15:20 -0600

    Update count_orderby.sql

--- Sakila_Practice/count_orderby.sql
+++ Sakila_Practice/count_orderby.sql
@@ -8,7 +8,6 @@ limit 5;
 select * from customer 
 limit 1;
 
-
 select first_name, last_name from customer
 order by first_name ASC; 
 
@@ -21,13 +20,37 @@ order by last_name ASC;
 select first_name, last_name from customer
 order by last_name DESC; 
 
-
-
 select first_name, last_name from customer
 order by first_name ASC, --if first name is the same, last name descending ordering takes effect
 last_name DESC;
 
-
 select first_name, last_name from customer
 order by last_name ASC, --if last name is the same, first name descending ordering takes effect
 first_name DESC;
+
+--possible in PostgreSQL
+select first_name 
+from customer
+order by last_name; --order by last name even if you don't want that in your results
+
+select first_name, last_name 
+from customer
+order by last_name; 
+
+select customer_id, amount 
+from payment
+order by amount DESC
+limit 10;
+
+--subquery
+select * 
+from customer
+where customer_id in (select customer_id from payment
+order by amount DESC
+limit 10)
+order by first_name;
+
+select * 
+from film
+order by film_id
+limit 5;

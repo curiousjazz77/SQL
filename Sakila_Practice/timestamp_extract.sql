fed0d99cf98b1fe99ec8b648baa665d30fa5af2e
Jazz <10506008+curiousjazz77@users.noreply.github.com>
2018-11-10 16:52:19 -0600

    Create timestamp_extract.sql

---
+++ Sakila_Practice/timestamp_extract.sql
@@ -0,0 +1,25 @@
+
+select * from payment
+limit 3;
+
+select payment_date 
+from payment;
+
+select extract(day from payment_date) 
+from payment;
+
+select extract(month from payment_date) 
+from payment;
+
+select extract(year from payment_date) 
+from payment;
+
+select customer_id, extract(day from payment_date) 
+from payment;
+
+select sum(amount), extract(month from payment_date) as month 
+from payment
+group by month
+order by sum(amount) desc
+limit 1
+;

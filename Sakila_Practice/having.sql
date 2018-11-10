b3ec06bee230fce7d4286ae2fd74f06810b3bc27
Jazz <10506008+curiousjazz77@users.noreply.github.com>
2018-11-09 22:38:55 -0600

    Create having.sql

---
+++ Sakila_Practice/having.sql
@@ -0,0 +1,45 @@
+
+--Having has usage for aggregate functions and always comes at end after group by statement if used. 
+select customer_id, sum(amount)
+from payment
+group by customer_id
+having sum(amount) > 200;
+
+select store_id, count(customer_id)
+from customer
+group by store_id
+having count(customer_id) > 300;
+
+select rating, rental_rate 
+from film
+where rating in ('R', 'G', 'PG');
+
+select rating, avg(rental_rate) 
+from film
+where rating in ('R', 'G', 'PG')
+group by rating;
+
+select rating, avg(rental_rate) 
+from film
+where rating in ('R', 'G', 'PG')
+group by rating
+having avg(rental_rate) < 3;
+
+
+select rating, avg(rental_rate) 
+from film
+group by rating
+having avg(rental_rate) < 3;
+
+
+
+
+
+
+
+
+
+
+
+
+

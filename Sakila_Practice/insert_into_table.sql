3fd5a3022b1e1829c70ea491f0fc965c7a5c3b2f
Jazz <10506008+curiousjazz77@users.noreply.github.com>
2018-11-10 18:41:00 -0600

    Create insert_into_table.sql

---
+++ Sakila_Practice/insert_into_table.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE link(
+ID serial PRIMARY KEY,
+url VARCHAR(255) NOT NULL, 
+name VARCHAR(255) NOT NULL,
+description VARCHAR(255),
+rel VARCHAR(50)
+);
+
+select * from link; 
+
+insert into link(url, name)
+VALUES
+('www.google.com', 'Google');
+
+select * from link;
